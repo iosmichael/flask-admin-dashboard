2fa4aa919396b07b577819792e97b08d3846c59a
EC2 Default User <ec2-user@ip-172-31-9-229.us-west-1.compute.internal>
2018-09-09 06:56:48 +0000

    modified bash scripts

--- start_cron_twilio.sh
+++ start_cron_twilio.sh
@@ -1,6 +1,6 @@
-cd /Users/michaelliu/Documents/Web\ Project/twilio/admin-dashboard/admin
+cd ~/admin
 export FLASK_APP=cron_twilio.py
 export FLASK_ENV=production
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
-/Users/michaelliu/anaconda/bin/flask run --host=0.0.0.0 --port=5001
+/usr/local/bin/flask run --host=0.0.0.0 --port=5001
